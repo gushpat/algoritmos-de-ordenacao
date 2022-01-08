d17801176667b8e6503f3ee2dd5865c34f27a8b3
ghpvampiro <gustavo_hp@hotmail.com>
2022-01-07 21:26:44 -0300

    Update algoritmodeordenacao.pas

--- Pascal/AlgoritmosdeOrdenacao/algoritmodeordenacao.pas
+++ Pascal/AlgoritmosdeOrdenacao/algoritmodeordenacao.pas
@@ -32,25 +32,301 @@ begin
   b := temp;
 end;
 
-
-procedure BubbleSort( var vetBubble: vArray );
+procedure SelectionSort( var vetSelection: vArray);
 var
-  n, newn, i:integer;
+  i, j, min, aux: Integer;
 begin
-  n := high( vetBubble );
-  repeat
-    newn := 0;
-    for i := 1 to n   do
-      begin
-        if vetBubble[ i - 1 ] > vetBubble[ i ] then
-          begin
-            swap( vetBubble[ i - 1 ], vetBubble[ i ]);
-            newn := i ;
-          end;
-      end ;
-    n := newn;
-  until n = 0;
+  for i := 1 to size - 1 do
+  begin
+    min := i;
+    for j := i + 1 to size do
+    begin
+      if vetSelection[j] < vetSelection[min] then
+        min := j;
+    end;
+    aux := vetSelection[i];
+    vetSelection[i] := vetSelection[min];
+    vetSelection[min] := aux;
+  end;
 end;
+
+
+procedure BubbleSort( var vetBubble: vArray);
+var
+  i, j, aux: Integer;
+begin
+  for i := 1 to size - 1 do
+  begin
+    for j := 1 to size - i do
+    begin
+      if vetBubble[j] > vetBubble[j + 1] then
+      begin
+        aux := vetBubble[j];
+        vetBubble[j] := vetBubble[j + 1];
+        vetBubble[j + 1] := aux;
+      end;
+    end;
+  end;
+end;
+
+procedure InsertionSort( var vetInsertion: vArray);
+var
+  i, j, aux: Integer;
+begin
+  for i := 2 to size do
+  begin
+    aux := vetInsertion[i];
+    j := i - 1;
+    while (j > 0) and (vetInsertion[j] > aux) do
+    begin
+      vetInsertion[j + 1] := vetInsertion[j];
+      j := j - 1;
+    end;
+    vetInsertion[j + 1] := aux;
+  end;
+end;
+
+procedure MergeSort( var vetMerge: vArray);
+var
+  i, j, k, m, n, q, r: Integer;
+  vetAux: vArray;
+begin
+  m := 0;
+  n := 0;
+  q := 0;
+  r := 0;
+  for i := 1 to size do
+  begin
+    if (vetMerge[i] < vetMerge[i + 1]) then
+    begin
+      m := m + 1;
+      vetAux[m] := vetMerge[i];
+    end
+    else
+    begin
+      n := n + 1;
+      vetAux[n] := vetMerge[i];
+    end;
+  end;
+  for i := 1 to m do
+  begin
+    vetMerge[i] := vetAux[i];
+  end;
+  for i := m + 1 to m + n do
+  begin
+    vetMerge[i] := vetAux[i];
+  end;
+  for i := 1 to m do
+  begin
+    for j := 1 to n do
+    begin
+      if (vetMerge[i] < vetMerge[i + j]) then
+      begin
+        q := q + 1;
+        vetAux[q] := vetMerge[i];
+      end
+      else
+      begin
+        r := r + 1;
+        vetAux[r] := vetMerge[i + j];
+      end;
+    end;
+  end;
+  for i := 1 to q do
+  begin
+    vetMerge[i] := vetAux[i];
+  end;
+  for i := q + 1 to q + r do
+  begin
+    vetMerge[i] := vetAux[i];
+  end;
+end;
+
+procedure QuickSort( var vetQuick: vArray);
+var
+  i, j, k, m, n, q, r: Integer;
+  vetAux: vArray;
+begin
+  m := 0;
+  n := 0;
+  q := 0;
+  r := 0;
+  for i := 1 to size do
+  begin
+    if (vetQuick[i] < vetQuick[i + 1]) then
+    begin
+      m := m + 1;
+      vetAux[m] := vetQuick[i];
+    end
+    else
+    begin
+      n := n + 1;
+      vetAux[n] := vetQuick[i];
+    end;
+  end;
+  for i := 1 to m do
+  begin
+    vetQuick[i] := vetAux[i];
+  end;
+  for i := m + 1 to m + n do
+  begin
+    vetQuick[i] := vetAux[i];
+  end;
+  for i := 1 to m do
+  begin
+    for j := 1 to n do
+    begin
+      if (vetQuick[i] < vetQuick[i + j]) then
+      begin
+        q := q + 1;
+        vetAux[q] := vetQuick[i];
+      end
+      else
+      begin
+        r := r + 1;
+        vetAux[r] := vetQuick[i + j];
+      end;
+    end;
+  end;
+  for i := 1 to q do
+  begin
+    vetQuick[i] := vetAux[i];
+  end;
+  for i := q + 1 to q + r do
+  begin
+    vetQuick[i] := vetAux[i];
+  end;
+end;
+
+procedure ShellSort( var vetShell: vArray);
+var
+  i, j, k, m, n, q, r: Integer;
+  vetAux: vArray;
+begin
+  m := 0;
+  n := 0;
+  q := 0;
+  r := 0;
+  for i := 1 to size do
+  begin
+    if (vetShell[i] < vetShell[i + 1]) then
+    begin
+      m := m + 1;
+      vetAux[m] := vetShell[i];
+    end
+    else
+    begin
+      n := n + 1;
+      vetAux[n] := vetShell[i];
+    end;
+  end;
+  for i := 1 to m do
+  begin
+    vetShell[i] := vetAux[i];
+  end;
+  for i := m + 1 to m + n do
+  begin
+    vetShell[i] := vetAux[i];
+  end;
+end;
+
+procedure HeapSort( var vetHeap: vArray);
+var
+  i, j, k, m, n, q, r: Integer;
+  vetAux: vArray;
+begin
+  m := 0;
+  n := 0;
+  q := 0;
+  r := 0;
+  for i := 1 to size do
+  begin
+    if (vetHeap[i] < vetHeap[i + 1]) then
+    begin
+      m := m + 1;
+      vetAux[m] := vetHeap[i];
+    end
+    else
+    begin
+      n := n + 1;
+      vetAux[n] := vetHeap[i];
+    end;
+  end;
+  for i := 1 to m do
+  begin
+    vetHeap[i] := vetAux[i];
+  end;
+  for i := m + 1 to m + n do
+  begin
+    vetHeap[i] := vetAux[i];
+  end;
+end;
+
+procedure CountingSort( var vetCounting: vArray);
+var
+  i, j, k, m, n, q, r: Integer;
+  vetAux: vArray;
+begin
+  m := 0;
+  n := 0;
+  q := 0;
+  r := 0;
+  for i := 1 to size do
+  begin
+    if (vetCounting[i] < vetCounting[i + 1]) then
+    begin
+      m := m + 1;
+      vetAux[m] := vetCounting[i];
+    end
+    else
+    begin
+      n := n + 1;
+      vetAux[n] := vetCounting[i];
+    end;
+  end;
+  for i := 1 to m do
+  begin
+    vetCounting[i] := vetAux[i];
+  end;
+  for i := m + 1 to m + n do
+  begin
+    vetCounting[i] := vetAux[i];
+  end;
+end;
+
+procedure RadixSort( var vetRadix: vArray);
+var
+  i, j, k, m, n, q, r: Integer;
+  vetAux: vArray;
+begin
+  m := 0;
+  n := 0;
+  q := 0;
+  r := 0;
+  for i := 1 to size do
+  begin
+    if (vetRadix[i] < vetRadix[i + 1]) then
+    begin
+      m := m + 1;
+      vetAux[m] := vetRadix[i];
+    end
+    else
+    begin
+      n := n + 1;
+      vetAux[n] := vetRadix[i];
+    end;
+  end;
+  for i := 1 to m do
+  begin
+    vetRadix[i] := vetAux[i];
+  end;
+  for i := m + 1 to m + n do
+  begin
+    vetRadix[i] := vetAux[i];
+  end;
+end;
+
+
 
 BEGIN
 
@@ -87,12 +363,23 @@ begin
   WriteLn(vet[CONT]);
 end;
 
-BubbleSort(vet);
+  END.
 
-FOR CONT:=1 to 10 do
+
+
+if opcao = 1 then
 begin
-  WriteLn(vet[CONT]);
+  BubbleSort(vetBubble);
+  FOR CONT:=1 to 10 do
+  begin
+    WriteLn(vetBubble[CONT]);
+  end;
 end;
+
+
+
+
+
 
 ReadLn();
 
